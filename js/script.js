92f11457f14a83c3615d185e10ec863a5b048d70
Yana <m.yana93@mail.ru>
2019-09-30 12:50:55 +0300

    Стилизация каталога + выпадающее меню + список товаров в корзине

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// let basket_btn = document.querySelector("navigation-btn-basket");
+//
+// basket_btn.onmouseover = function() {
+//   console.log('ON');
+//   basket_btn.classList.add("show");
+// }
+//
+// basket_btn.onmouseout = function() {
+//   basket_btn.classList.add("visually-hidden");
+// }
