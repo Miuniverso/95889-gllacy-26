52476d41f7f7310a25a954f405dce161d2e10acb
Yana <m.yana93@mail.ru>
2019-10-10 00:27:47 +0300

    Корректировки
    + Ховер эффект у мороженого на главной странице
    + настройка выпадающих меню и форм в навигации
    - "ХИТ" перекрывается картинкой мороженого
    + Открытие попапа с формой обратной связи

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
 let feedbackBtn = document.querySelector(".information-btn");
 let feedbackForm = document.querySelector(".feedback-form");
+let overlay = document.querySelector(".overlay");
 let closeFormBtn = document.querySelector(".feedback-close-btn");
-let paginatorItems = document.querySelectorAll(".paginator-item");
+// let paginatorItems = document.querySelectorAll(".paginator-item");
 
-//
-// feedbackBtn.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-//   feedbackForm.classList.remove("visually-hidden");
-// })
-//
-// closeFormBtn.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-//   feedbackForm.classList.add("visually-hidden");
-// })
 
+feedbackBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackForm.classList.remove("visually-hidden");
+  overlay.classList.remove("visually-hidden");
+})
+
+closeFormBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackForm.classList.add("visually-hidden");
+  overlay.classList.add("visually-hidden");
+})
+
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackForm.classList.add("visually-hidden");
+  overlay.classList.add("visually-hidden");
+})
 
 // что-то я не то делаю. Добавить класс могу, но не могу убрать его
 // у предыдущей выбранной ссылки :(
 
-paginatorItems.forEach(item => item.addEventListener("click", function() {
-  console.log("Before");
-  item.classList.remove("paginator-item-current");
-  console.log("Delete for all");
-  this.classList.add("paginator-item-current");
-  console.log("After");
-}));
+// paginatorItems.forEach(item => item.addEventListener("click", function() {
+//   console.log("Before");
+//   item.classList.remove("paginator-item-current");
+//   console.log("Delete for all");
+//   this.classList.add("paginator-item-current");
+//   console.log("After");
+// }));
