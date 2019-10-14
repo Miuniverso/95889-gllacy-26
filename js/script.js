0882e4dc499f7df609ebfe13a2002dafe07f0cd1
Yana <m.yana93@mail.ru>
2019-10-14 10:16:12 +0300

    Ковыряния в JS

--- js/script.js
+++ js/script.js
@@ -2,13 +2,39 @@ let feedbackBtn = document.querySelector(".information-btn");
 let feedbackForm = document.querySelector(".feedback-form");
 let overlay = document.querySelector(".overlay");
 let closeFormBtn = document.querySelector(".feedback-close-btn");
-// let paginatorItems = document.querySelectorAll(".paginator-item");
+let form = document.querySelector(".feedback-form");
+let name = feedbackForm.querySelector("[name=name-last-name-input]");
+let email = feedbackForm.querySelector("[name=email-comment-input]");
+let comment = feedbackForm.querySelector("[name=comment-input]");
+
+let isStorageSupport = true;
+let storage = "";
+
+ try {
+   storageName = localStorage.getItem("name");
+   storageEmail = localStorage.getItem("email");
+ } catch (err) {
+   isStorageSupport = false;
+ }
 
 
 feedbackBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackForm.classList.remove("visually-hidden");
   overlay.classList.remove("visually-hidden");
+  if (storageName) {
+      name.value = storageName;
+      email.focus();
+    } else {
+      name.focus();
+    }
+
+    if (storageEmail) {
+        email.value = storageEmail;
+        comment.focus();
+      } else {
+        email.focus();
+      }
 })
 
 closeFormBtn.addEventListener("click", function (evt) {
@@ -17,20 +43,31 @@ closeFormBtn.addEventListener("click", function (evt) {
   overlay.classList.add("visually-hidden");
 })
 
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (!feedbackForm.classList.contains("visually-hidden")) {
+        console.log("Форма открыта")
+        feedbackForm.classList.add("visually-hidden");
+        overlay.classList.add("visually-hidden");
+      }
+    }
+  });
+
+form.addEventListener("submit", function(evt) {
+  if (!name.value || !email.value || !comment.value) {
+      evt.preventDefault();
+      console.log("Нужно заполнить все поля");
+    } else {
+      if(isStorageSupport) {
+        localStorage.setItem("name", name.value);
+        localStorage.setItem("email", email.value);
+      }
+    }
+})
 
 overlay.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackForm.classList.add("visually-hidden");
   overlay.classList.add("visually-hidden");
 })
-
-// что-то я не то делаю. Добавить класс могу, но не могу убрать его
-// у предыдущей выбранной ссылки :(
-
-// paginatorItems.forEach(item => item.addEventListener("click", function() {
-//   console.log("Before");
-//   item.classList.remove("paginator-item-current");
-//   console.log("Delete for all");
-//   this.classList.add("paginator-item-current");
-//   console.log("After");
-// }));
